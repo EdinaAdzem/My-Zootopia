bd9f3b986c5673976730933d4050a5412ebfcdc9
edina adzem <edina.plakalo@gmail.com>
2024-06-10 11:53:09 +0200

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,6 +5,7 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+
 def display_animal_data():
     """read and print the animal_data contents"""
     data = load_data("animals_data.json")
@@ -14,7 +15,7 @@ def display_animal_data():
         if "characteristics" in animal and "diet" in animal["characteristics"]:
             print(f"Diet: {animal['characteristics']['diet']}")
         if "locations" in animal:
-            print(f"First Location: {animal['locations'][0]}")
+            print(f"Location: {animal['locations'][0]}")
         if "characteristics" in animal and "type" in animal["characteristics"]:
             print(f"Type: {animal['characteristics']['type']}")
         print()
