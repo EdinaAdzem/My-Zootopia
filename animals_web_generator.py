d3e7d33e93831108a699cc05ac354060d66cdc61
edina adzem <edina.plakalo@gmail.com>
2024-06-11 10:44:11 +0200

    bring animal data to a new animal.html file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,21 +5,47 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-
-def display_animal_data():
+def display_animal_data(data):
     """read and print the animal_data contents"""
-    data = load_data("animals_data.json")
+    output = ''
     for animal in data:
         if "name" in animal:
-            print(f"Name: {animal['name']}")
+            output += f"Name: {animal['name']}"
         if "characteristics" in animal and "diet" in animal["characteristics"]:
-            print(f"Diet: {animal['characteristics']['diet']}")
+            output += f"Diet: {animal['characteristics']['diet']}"
         if "locations" in animal:
-            print(f"Location: {animal['locations'][0]}")
+            output += f"Location: {animal['locations'][0]}"
         if "characteristics" in animal and "type" in animal["characteristics"]:
-            print(f"Type: {animal['characteristics']['type']}")
-        print()
+            output += f"Type: {animal['characteristics']['type']}"
+        output += "<br/>\n"
+    return output
 
-display_animal_data()
+def load_template(template_path):
+    """Read and return the content of the HTML template"""
+    with open(template_path, "r") as template_file:
+        return template_file.read()
 
+def replace_placeholder(template_content, animals_info):
+    """Replace the placeholder in the template with the animals' data"""
+    return template_content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
 
+def write_html(content, output_path):
+    """Write the new HTML content to a file"""
+    with open(output_path, "w") as output_file:
+        output_file.write(content)
+
+if __name__ == "__main__":
+    # Load the animal data
+    animals_data = load_data("animals_data.json")
+
+    # Load the template content
+    template_new_content = load_template("animals_template.html")
+
+    # Generate the animals information
+    animals_info = display_animal_data(animals_data)
+
+    # replace wtih animal info
+    new_animal_info_content = replace_placeholder(template_new_content, animals_info)
+
+    # Write the new content to the output file
+    write_html(new_animal_info_content, "animals.html")
